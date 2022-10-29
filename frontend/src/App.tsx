df1d699da326215d91bd500b2d4a9d784842472e
Kim Hyein <cozitive.dev@gmail.com>
2022-10-29 11:39:00 +0900

    Add: routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import SignIn from './page/SignIn';
+import SignUp from './page/SignUp';
+import Search from './page/Search';
+import ProfileDetail from './page/ProfileDetail';
+import ChatList from './page/ChatList';
+import ChatDetail from './page/ChatDetail';
+import PitapatRequest from './page/PitapatRequest';
+import Setting from './page/Setting';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/signin' element={<SignIn />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/serach' element={<Search />} />
+          <Route path='/profile/:id' element={<ProfileDetail />} />
+          <Route path='/chat' element={<ChatList />} />
+          <Route path='/chat/:id' element={<ChatDetail />} />
+          <Route path='/pitapat' element={<PitapatRequest />} />
+          <Route path='/setting' element={<Setting />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
