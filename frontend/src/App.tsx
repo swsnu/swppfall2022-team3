520c7e6028330455080557856ec2289458207a80
kl7407 <eee7407@naver.com>
2022-10-29 12:05:54 +0900

    Upadte: typo and default route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import SignIn from './page/SignIn';
 import SignUp from './page/SignUp';
 import Search from './page/Search';
@@ -15,12 +15,13 @@ function App() {
         <Routes>
           <Route path='/signin' element={<SignIn />} />
           <Route path='/signup' element={<SignUp />} />
-          <Route path='/serach' element={<Search />} />
+          <Route path='/search' element={<Search />} />
           <Route path='/profile/:id' element={<ProfileDetail />} />
           <Route path='/chat' element={<ChatList />} />
           <Route path='/chat/:id' element={<ChatDetail />} />
           <Route path='/pitapat' element={<PitapatRequest />} />
           <Route path='/setting' element={<Setting />} />
+          <Route path='/*' element={<Navigate replace to={"/signin"}/>} />
         </Routes>
       </BrowserRouter>
     </div>
