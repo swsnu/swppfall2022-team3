705c6339baee4149770e15503faedc13f4397307
Kim Hyein <cozitive.dev@gmail.com>
2022-11-03 07:26:27 +0900

    Fix: load elements by key

--- frontend/src/page/ProfileDetail.tsx
+++ frontend/src/page/ProfileDetail.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
-import { users } from '../DummyData';
+import { photos, tags, users } from '../DummyData';
+import { useNavigate } from 'react-router-dom';
 import { getKoreanAge, User } from '../types';
 
 export default function ProfileDetail() {
+  const navigate = useNavigate();
   const { id } = useParams();
   const [user, setUser] = useState<User>();
 
@@ -13,8 +15,8 @@ export default function ProfileDetail() {
 
   return user ? (
     <div>
-      <div id='topbar' className='flex justify-center py-2'>
-        <button className='flex-none px-4 text-lg'>B</button>
+      <div id='appbar' className='flex justify-center py-2'>
+        <button onClick={() => navigate(-1)} className='flex-none px-4 text-lg'>B</button>
         <div className='flex-auto flex justify-center'>
           <div className='flex-none text-center text-lg font-bold'>
             {user.username}/{getKoreanAge(user.birthday)}
@@ -23,18 +25,21 @@ export default function ProfileDetail() {
         </div>
         <button className='flex-none px-4 text-lg'>P</button>
       </div>
-<<<<<<< HEAD
-      <img id='repr-photo' src={user.photos[0]} alt='repr' />
-=======
       <div id='scroll' className='flex overflow-x-auto snap-x snap-mandatory'>
-        {user.photos.map((photo, index) =>
-          <img key={index} className='snap-center' src={photo} alt={photo} />
+        {user.photos.map((p, index) =>
+          <img
+            key={index}
+            className='snap-center w-100%'
+            src={photos.find((photo) => photo.key === p)?.path}
+            alt={String(photos.find((photo) => photo.key === p)?.index)}
+          />
         )}
       </div>
->>>>>>> 84f3ad9 (Add: photo horizontal scroll)
       <div id='tags' className='flex flex-wrap m-1.5 text-sm font-bold text-pink-500'>
-        {user.tags.map((tag, index) =>
-          <div key={index} className='flex-none px-2.5 py-0.5 mx-1 my-1 rounded-2xl border-2 border-pink-400'>{tag}</div>
+        {user.tags.map((t, index) =>
+          <div key={index} className='flex-none px-2.5 py-0.5 mx-1 my-1 rounded-2xl border-2 border-pink-400'>
+            {tags.find((tag) => tag.key === t)?.name}
+          </div>
         )}
       </div>
       <div id='intro' className='mx-3 my-2 text-sm'>{user.introduction}</div>
