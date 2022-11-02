616bf9547a9b817d69a22e952e914b7a1c286054
kl7407 <eee7407@naver.com>
2022-11-03 00:58:03 +0900

    Update: types

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,53 +1,59 @@
-export enum Gender { MALE, FEMALE };
+export enum Gender {
+  MALE = 'M',
+  FEMALE = 'F',
+}
 
 export interface Major {
   key: number;
   name: string;
-};
+}
 
 export interface College {
   key: number;
   name: string;
-  majors: Major[];
-};
+  majors: number[];
+}
 
 export interface University {
   key: number;
   name: string;
-  colleges: College[];
-};
+  colleges: number[];
+}
 
 export interface Tag {
   key: number;
   name: string;
   type: string;
-};
+}
 
 export interface Photo {
   key: number;
   user_key: number;
   index: number;
   path: string;
-};
+}
 
-export interface BaseUser {
+interface BaseUser {
   key: number;
   email: string;
   username: string;
   gender: Gender;
   birthday: Date;
   location: string;
-  university: string;
-  college: string;
-  major: string;
-  reprPhoto: string;
-};
+  university: number;
+  college: number;
+  major: number;
+}
+
+export interface SimplifiedUser extends BaseUser{
+  reprPhoto: number;
+}
 
 export interface User extends BaseUser {
   introduction: string;
-  tags: string[];
-  photos: string[];
-};
+  tags: number[];
+  photos: number[];
+}
 
 export function getKoreanAge(birthday: Date): number {
   return (new Date().getFullYear() - birthday.getFullYear() + 1);
