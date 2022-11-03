6643f2b6a69d89bd3daccaa271821c8d8fb18f50
pacifiic <yellowbill@snu.ac.kr>
2022-11-03 22:07:38 +0900

    Fix: PitapatRequest (indentation)

--- frontend/src/page/PitapatRequest.tsx
+++ frontend/src/page/PitapatRequest.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
-import {photos, fakeBaseUsers, pitapats, fakeBaseUser} from '../DummyData';
+import { photos, fakeBaseUsers, pitapats, fakeBaseUser } from '../DummyData';
 import { getKoreanAge, Page } from '../types';
 import Profile from "../component/Profile";
 import NavigationBar from "../component/NavigationBar";
@@ -11,6 +11,8 @@ import {isBooleanObject} from "util/types";
 
 export default function PitapatRequest() {
   const navigate = useNavigate();
+  // when Req is true -> 내가 받은 두근
+  // when Req is false -> 내가 보낸 두근
   const [isItReq, setIsItReq] = useState<boolean>(true);
 
   const showReqHandler = () => {
@@ -59,24 +61,24 @@ export default function PitapatRequest() {
               return null;
           })}
         </div>
-          :
-          <div id="sent-list">
-            {pitapats.filter(pitapat => pitapat.from === 1).map((pitapat) => {
-              const toUser = fakeBaseUsers.find(user => user.key === pitapat.to);
-              if(toUser) {
-                return (
-                  <Profile key={toUser.key}
-                           username={toUser.username}
-                           koreanAge={getKoreanAge(toUser.birthday)}
-                           photo={photos.find((p) => p.key === toUser.reprPhoto)?.path}
-                           clickDetail={() => clickUserHandler(toUser.key)}
-                  />
-                )
-              }
-              else
-                return null;
-            })}
-          </div>
+        :
+        <div id="sent-list">
+          {pitapats.filter(pitapat => pitapat.from === 1).map((pitapat) => {
+            const toUser = fakeBaseUsers.find(user => user.key === pitapat.to);
+            if(toUser) {
+              return (
+                <Profile key={toUser.key}
+                         username={toUser.username}
+                         koreanAge={getKoreanAge(toUser.birthday)}
+                         photo={photos.find((p) => p.key === toUser.reprPhoto)?.path}
+                         clickDetail={() => clickUserHandler(toUser.key)}
+                />
+              )
+            }
+            else
+              return null;
+          })}
+        </div>
       }
       <NavigationBar selectedPath={2}/>
     </div>
