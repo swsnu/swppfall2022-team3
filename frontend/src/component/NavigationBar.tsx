63618da0232cafdecc7847255bf0296608877ae0
kl7407 <eee7407@naver.com>
2022-11-04 13:14:27 +0900

    Fix: strange bug that icon class name is not applied when there is a formatted string

--- frontend/src/component/NavigationBar.tsx
+++ frontend/src/component/NavigationBar.tsx
@@ -9,8 +9,6 @@ import path from "../constant/path";
  */
 type TabIndex = 0 | 1 | 2 | -1;
 
-const tabColor = "white";
-const defaultIconClassName = `h-8 w-8 stroke-1 stroke-${tabColor}`;
 const tabClassName = "flex-1 flex items-center justify-center";
 
 const NavigationBar = () => {
@@ -37,6 +35,19 @@ const NavigationBar = () => {
 
   }, [navigate, selectedTab]);
 
+  const getIconClassName = useCallback((index: TabIndex): string => {
+    // const defaultIconClassName = `h-8 w-8 stroke-1 stroke-${tabColor}`;
+    if (selectedTab === index)
+      return "h-8 w-8 stroke-1 stroke-white fill-white";
+    else
+      return "h-8 w-8 stroke-1 stroke-white fill-none";
+  }, [selectedTab]);
+
+  const getStroke = useCallback(
+    (index: TabIndex): string => selectedTab === index ? "fill-white" : "",
+    [selectedTab]
+  );
+
   useEffect(() => {
     let newSelectedTab: TabIndex = -1;
     if (pathName.startsWith(path.search)) {
@@ -58,8 +69,8 @@ const NavigationBar = () => {
         onClick={(() => { tabOnClickListener(0) })}
       >
         <UserIcon
-          className={`${defaultIconClassName} ${selectedTab === 0 ? `fill-${tabColor}` : "fill-none"}`}
-          stroke={selectedTab === 0 ? `fill-${tabColor}` : undefined}
+          className={getIconClassName(0)}
+          stroke={getStroke(0)}
         />
       </button>
       <button
@@ -67,8 +78,8 @@ const NavigationBar = () => {
         onClick={(() => { tabOnClickListener(1) })}
       >
         <HeartIcon
-          className={`${defaultIconClassName} ${selectedTab === 1 ? `fill-${tabColor}` : "fill-none"}`}
-          stroke={selectedTab === 1 ? `fill-${tabColor}` : undefined}
+          className={getIconClassName(1)}
+          stroke={getStroke(1)}
         />
       </button>
       <button
@@ -76,8 +87,8 @@ const NavigationBar = () => {
         onClick={(() => { tabOnClickListener(2) })}
       >
         <ChatBubbleLeftIcon
-          className={`${defaultIconClassName} ${selectedTab === 2 ? `fill-${tabColor}` : "fill-none"}`}
-          stroke={selectedTab === 2 ? `fill-${tabColor}` : undefined}
+          className={getIconClassName(2)}
+          stroke={getStroke(2)}
         />
       </button>
     </div>
