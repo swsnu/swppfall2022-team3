00ebaa17fb64827f49dc5a562e2c72c097118af0
pacifiic <yellowbill@snu.ac.kr>
2022-11-06 21:31:40 +0900

    Add: user.ts (login)

--- frontend/src/store/slices/user.ts
+++ frontend/src/store/slices/user.ts
@@ -1,10 +1,18 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { User } from "../../types";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import {Gender, User} from "../../types";
 import dummyData from "../../dummyData";
+import axios from "axios";
 import { RootState } from "../index";
+import {init} from "emailjs-com";
 
 
 const storeKey = "user";
+
+export interface UserState {
+  users: User[];
+  loginUser: User | null;
+}
+
 
 const getInitialUsers = (): User[] => {
   let savedValue = localStorage.getItem(storeKey);
@@ -15,12 +23,28 @@ const getInitialUsers = (): User[] => {
   return (JSON.parse(savedValue!) as any[]).map((user) => ({...user, birthday: new Date(user.birthday)})) as User[];
 };
 
+const initialState: UserState = {
+  users: getInitialUsers(),
+  loginUser: null,
+};
+
+export const login = createAsyncThunk(
+  "user/",
+  async (user: { email: string, password: string }, { dispatch }) => {
+    const response = await axios.post(`/api/user/login/`, user);
+    if (response.status === 204) {
+      dispatch(userActions.login({ email: user.email }));
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+);
+
 const userSlice = createSlice({
   name: "user",
-  initialState: {
-    users: getInitialUsers(),
-    loginUser: null,
-  },
+  initialState,
   reducers: {
     add: (state, action: PayloadAction<User>) => {
       const newUsers = [...state.users, action.payload];
@@ -40,9 +64,13 @@ const userSlice = createSlice({
       localStorage.setItem(storeKey, JSON.stringify(newUsers));
       state.users = newUsers;
     },
+    login: (state, action: PayloadAction<{ email: string }>) => {
+      state.loginUser = state.users.find(user => user.email === action.payload.email) ?? null;
+    },
   },
 })
 
+export const userActions = userSlice.actions;
 export const selectUser = (state: RootState) => state.user;
 const userReducer = userSlice.reducer;
 export default userReducer;
