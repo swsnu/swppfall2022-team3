5db98684fe28128b7df7063664e79ad57b8f92ed
Kim Hyein <cozitive.dev@gmail.com>
2022-11-06 15:28:44 +0900

    Fix: square image in Profile

--- frontend/src/component/Profile.tsx
+++ frontend/src/component/Profile.tsx
@@ -14,15 +14,15 @@ export default function Profile({
   clickDetail,
 }: IProps) {
   return (
-    <div className="relative">
-      <button onClick={clickDetail}>
-        <img key={key} src={photo} alt={photo}/>
-        <div
-          className={"absolute bottom-0 left-0 right-0 px-4 py-2 flex text-white font-bold justify-center"}
-          style={{
-            background: "linear-gradient(180deg, rgba(0, 0, 0, 0.2) 0%, #000000 100%)",
-          }}
-        >
+    <div className="relative h-[100vw]">
+      <button onClick={clickDetail} className="w-full">
+        <img
+          key={key}
+          src={photo}
+          alt={photo}
+          className="object-cover w-full h-[100vw]"
+        />
+        <div className="absolute bottom-0 left-0 right-0 px-4 py-2 bg-gray-800 opacity-70 flex text-white font-bold justify-center">
           {username}/{koreanAge}
         </div>
       </button>
