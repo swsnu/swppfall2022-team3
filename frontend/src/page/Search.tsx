2bcc956e7d58a4ad44125c7ea54ff58b78317a8e
Kim Hyein <cozitive.dev@gmail.com>
2022-11-06 23:53:48 +0900

    Fix: navigate in useEffect

--- frontend/src/page/Search.tsx
+++ frontend/src/page/Search.tsx
@@ -1,4 +1,4 @@
-import { getKoreanAge, PitapatStatus, User } from '../types';
+import { getKoreanAge } from '../types';
 import Profile from "../component/Profile";
 import NavigationBar from "../component/NavigationBar";
 import AppBar from "../component/AppBar";
@@ -8,41 +8,43 @@ import { selectPhoto } from "../store/slices/photo";
 import { selectPitapat } from "../store/slices/pitapat";
 import path from "../constant/path";
 import { getPitapatStatus } from "../util/getPitapatStatus";
-import { Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from 'react';
 
 
 export default function Search() {
-  const loginUser = useSelector(selectUser).loginUser;
-  const myKey = loginUser?.key ?? null;
+  const navigate = useNavigate();
   const users = useSelector(selectUser).users;
-  const photos = useSelector(selectPhoto).photos;
+  const loginUser = useSelector(selectUser).loginUser;
   const pitapats = useSelector(selectPitapat).pitapats;
+  const photos = useSelector(selectPhoto).photos;
 
+  useEffect(() => {
+    if (loginUser === null) {
+      navigate(path.signIn);
+    }
+  }, [navigate, loginUser]);
 
-  if (myKey === null) {
-    return <Navigate to={path.signIn} />;
-  }
-  else {
-    return (
-      <section className={"mt-12 mb-16 w-full"}>
-        <AppBar/>
-        <section>
-          {users.filter(user => user.key !== myKey).map((user) => {
-            return (
-              <Profile
-                key={user.key}
-                myKey={myKey}
-                userKey={user.key}
-                username={user.username}
-                koreanAge={getKoreanAge(user.birthday)}
-                photo={photos.find((p) => p.key === user.photos[0])?.path!}
-                status={getPitapatStatus(user, myKey, pitapats)}
-              />
-            );
-          })}
-        </section>
-        <NavigationBar/>
+  return (
+    loginUser ?
+    <section className={"mt-12 mb-16 w-full"}>
+      <AppBar/>
+      <section>
+        {users.filter(user => user.key !== loginUser!.key).map((user) => {
+          return (
+            <Profile
+              key={user.key}
+              myKey={loginUser!.key}
+              userKey={user.key}
+              username={user.username}
+              koreanAge={getKoreanAge(user.birthday)}
+              photo={photos.find((p) => p.key === user.photos[0])?.path!}
+              status={getPitapatStatus(user, loginUser!.key, pitapats)}
+            />
+          );
+        })}
       </section>
-    );
-  }
+      <NavigationBar/>
+    </section> : <section></section>
+  );
 }
