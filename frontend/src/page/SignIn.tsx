57d66a24fc58ffbaf7d3cdc14496cd61dcaed37a
kl7407 <eee7407@naver.com>
2022-11-06 16:31:46 +0900

    Update: refactor Signin

--- frontend/src/page/SignIn.tsx
+++ frontend/src/page/SignIn.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router";
 import { selectUser } from "../store/slices/user";
@@ -11,46 +11,59 @@ export default function SignIn() {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
 
-  const loginHandler = () => {
+  const loginOnClick = useCallback(() => {
     const thisUser = users.find((user) => (user.email === email));
     if (thisUser) {
       navigate(path.search);
     }
     else {
-      setEmail("");
-      setPassword("");
       alert("이메일이 틀렸습니다.");
     }
-  }
+  }, [email])
 
   return (
-    <section className="h-screen w-full flex flex-col mt-12 mb-16">
-      <h1 className="text-center text-5xl text-pink-500 font-bold">두근두근</h1>
-      <h1 className="text-center text-5xl text-pink-500 font-bold">캠퍼스</h1>
-      <div className="flex justify-center mt-24">
-        <label className="flex-none mx-8 my-1 text-center m-auto text-pink-500/100">{"이메일"}
-          <input className="flex-auto mx-8 my-1 w-48 border-solid border-b-4 border-l-2 border-r-2 rounded-md"
-            value={email}
-            onChange={(event) => setEmail(event.target.value)}></input>
-        </label>
+    <section className={"h-screen w-full flex flex-col mt-12 mb-16"}>
+      <h1 className={"text-center text-5xl text-pink-500 font-bold mt-16"}>
+        두근두근<br/>
+        캠퍼스
+      </h1>
+      <div className={"flex flex-row justify-center mt-28"}>
+        <article className={"flex-1 text-pink-500 font-bold text-center"}>
+          이메일:
+        </article>
+        <input
+          className={"ml-4 mr-8 my-1 w-48 border-solid border-b-4 border-l-2 border-r-2 rounded-md"}
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
       </div>
-      <div className="flex justify-center">
-        <label className="flex-none mx-8 my-1 text-center m-auto text-pink-500/100">{"비밀번호"}
-          <input className="flex-auto mx-8 my-1 w-48 border-solid border-b-4 border-l-2 border-r-2 rounded-md"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}></input>
-        </label>
+      <div className={"flex flex-row justify-center mb-24"}>
+        <article className={"flex-1 text-pink-500 font-bold text-center"}>
+          비밀번호:
+        </article>
+        <input
+          className={"ml-4 mr-8 my-1 w-48 border-solid border-b-4 border-l-2 border-r-2 rounded-md"}
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
       </div>
-      <div className="text-center mt-24">
+      <div className={"text-center mt-16"}>
         <div>
-          <button className="bg-white-500 text-center text-pink-400 border-solid border-b-4 border-l-2 border-r-2 w-36 h-8 rounded-md"
+          <button
+            className={"bg-white-500 text-center text-pink-400 border-solid border-b-4 border-l-2 border-r-2 w-36 h-8 rounded-md"}
             disabled={!email || !password}
-            onClick={() => loginHandler()}>
-            로그인</button>
+            onClick={loginOnClick}
+          >
+            로그인
+          </button>
         </div>
         <div>
-          <button className="bg-pink-500 text-center text-white mt-2 w-36 h-8 rounded-md"
-            onClick={() => navigate(path.signUp)}>회원가입</button>
+          <button
+            className={"bg-pink-500 text-center text-white mt-2 w-36 h-8 rounded-md"}
+            onClick={() => navigate(path.signUp)}
+          >
+            회원가입
+          </button>
         </div>
       </div>
     </section>
