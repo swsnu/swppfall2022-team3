e537ac1a4fe3db84383edb4b7c08a6e83f47bcdf
pacifiic <yellowbill@snu.ac.kr>
2022-11-06 14:26:26 +0900

    Update: Profile(component)

--- frontend/src/component/Profile.tsx
+++ frontend/src/component/Profile.tsx
@@ -14,11 +14,9 @@ export default function Profile({
   clickDetail,
 }: IProps) {
   return (
-    <div className="Profile">
-      <button onClick={clickDetail}>
-        <img key={key} className="snap-center" src={photo} alt={photo} />
-      </button>
-      <div>
+    <div className="relative">
+      <img key={key} src={photo} alt={photo}/>
+      <div className="absolute bottom-0 left-0 right-0 px-4 py-2 bg-gray-800 opacity-70 flex text-white font-bold justify-center">
         {username}/{koreanAge}
       </div>
     </div>
