d7222c43976b4641140b4d37338993a9075e9fb2
SINUUUU <tjd3507@snu.ac.kr>
2022-11-08 22:06:38 +0900

    Fix: Signin password hidden

--- frontend/src/page/SignIn.tsx
+++ frontend/src/page/SignIn.tsx
@@ -70,6 +70,7 @@ export default function SignIn() {
             }}
             size={"small"}
             label={""}
+            type={"password"}
             variant={"outlined"}
             value={password}
             onChange={(e) => {
