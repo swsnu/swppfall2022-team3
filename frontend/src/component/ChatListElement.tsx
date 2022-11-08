b7f51140c2fff2aa25374d4a2f443bfc5707255f
kl7407 <eee7407@naver.com>
2022-11-08 19:25:44 +0900

    Fix: ChatListElement text overflow

--- frontend/src/component/ChatListElement.tsx
+++ frontend/src/component/ChatListElement.tsx
@@ -34,23 +34,24 @@ export default function ChatListElement({
   }, [user, loginUser, navigate, photoPath]);
 
   return (
-    <article
-      role={"presentation"}
-      className={"w-full h-20 flex flex-row items-center border-b-2 border-b-gray-300"}
-    >
-      <button onClick={() => navigate("/profile/" + user.key)}>
+    <article className={"w-full h-20 flex flex-row items-center border-b-2 border-b-gray-300"}>
+      <button
+        className={"min-w-fit min-h-fit"}
+        onClick={() => navigate("/profile/" + user.key)}
+      >
         <img
           className={"w-16 h-16 m-2 bg-blue-100 rounded-full"}
           alt={""}
           src={photoPath}
         />
       </button>
-      <button
-        className={"flex-1 flex flex-col h-full pl-2"}
+      <article
+        role={"presentation"}
+        className={"flex-1 flex flex-col h-full pl-2 w-0"}
         onClick={elementOnClick}>
         <article className={"basis-1/4 pt-2 font-bold text-lg"}>{user.username}</article>
-        <article className={"basis-3/4 items-center text-gray-600"}>{lastChat ?? "대화를 시작해보세요!!"}</article>
-      </button>
+        <article className={"basis-3/4 items-center text-gray-600 truncate"}>{lastChat ?? "대화를 시작해보세요!!"}</article>
+      </article>
     </article>
   );
 }
