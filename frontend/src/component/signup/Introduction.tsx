c7ea7992beb311cb3c1f47402e503b67226a3710
kl7407 <eee7407@naver.com>
2022-11-09 12:02:13 +0900

    Fix: Introduction button position

--- frontend/src/component/signup/Introduction.tsx
+++ frontend/src/component/signup/Introduction.tsx
@@ -26,32 +26,30 @@ export default function Introduction({
 
   return (
     <section className={"flex flex-col items-center w-full"}>
-      <section className={"w-full h-[36rem] flex flex-col items-center"}>
-        <p className="mt-16 mb-8 text-center text-pink-500/100">
-          자신의 매력을 어필할 수 있는<br />
-          소개글을 작성해보세요!
-        </p>
-        <section className="w-[80vw]">
-          <div className={"text-center"}>
-            <textarea
-              className={"w-full h-[50vh] align-text-top border-solid border-b-4 border-l-2 border-r-2 rounded-md"}
-              placeholder="소개글을 작성해주세요!"
-              value={introduction}
-              onChange={(event) => setIntroduction(event.target.value)}
-            >
-            </textarea>
-          </div>
-        </section>
-        <article className={"ml-8 text-red-500 text-sm"}>{(hasSubmit) ? "필수 작성 항목입니다." : " "}</article>
+      <p className="mt-16 mb-8 text-center text-pink-500/100">
+        자신의 매력을 어필할 수 있는<br/>
+        소개글을 작성해보세요!
+      </p>
+      <section className="flex-1 flex flex-col w-full items-center">
+        <div className={"text-center w-4/5"}>
+          <textarea
+            className={"w-full h-[50vh] align-text-top border-solid border-b-4 border-l-2 border-r-2 rounded-md"}
+            placeholder="소개글을 작성해주세요!"
+            value={introduction}
+            onChange={(event) => setIntroduction(event.target.value)}
+          >
+          </textarea>
+        </div>
+        <article className={"text-red-500 text-sm w-4/5 indent-4"}>
+          {(hasSubmit) ? "필수 작성 항목입니다." : " "}
+        </article>
       </section>
-      <article className={"absolute bottom-12"}>
-        <button
-          className={"bg-pink-500 text-center text-white w-36 h-12 rounded-md"}
-          onClick={confirmOnClick}
-        >
-          다음
-        </button>
-      </article>
+      <button
+        className={"w-36 h-12 min-h-12 my-12 bg-pink-500 text-center text-white rounded-md"}
+        onClick={confirmOnClick}
+      >
+        다음
+      </button>
     </section>
   );
 }
