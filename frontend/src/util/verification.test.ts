20227c9f795a506bd33daf7b8e27601178213506
kl7407 <eee7407@naver.com>
2022-11-11 03:27:05 +0900

    Remove: unnecessary part to simplify the code

--- frontend/src/util/verification.test.ts
+++ frontend/src/util/verification.test.ts
@@ -2,11 +2,6 @@ import { getCode } from "./verification";
 
 
 describe("verification", () => {
-  beforeEach(() => {
-    jest.clearAllMocks();
-    global.window = Object.create(window);
-  });
-
   it("returns verification code", () => {
     Object.defineProperty(window, "crypto", {
       value: { getRandomValues: jest.fn().mockReturnValue(0) },
