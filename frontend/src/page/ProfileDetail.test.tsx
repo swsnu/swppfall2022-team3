2551b29bd62d9731db70d7e15bc2fe0c697c9f3c
Kim Hyein <cozitive.dev@gmail.com>
2022-11-10 15:52:03 +0900

    Add: ProfileDetail test

---
+++ frontend/src/page/ProfileDetail.test.tsx
@@ -0,0 +1,74 @@
+import * as React from "react";
+import { Provider } from "react-redux";
+import { MemoryRouter, Route, Routes } from "react-router";
+import { ToolkitStore } from "@reduxjs/toolkit/dist/configureStore";
+import { render, screen } from "@testing-library/react";
+import { chats, colleges, majors, photos, pitapats, tags, universities, users } from "../dummyData";
+import { getMockStore } from "../test-utils/mocks";
+import ProfileDetail from "./ProfileDetail";
+
+
+const mockNavigate = jest.fn();
+jest.mock("react-router", () => ({
+  ...jest.requireActual("react-router"),
+  useNavigate: () => mockNavigate,
+}));
+
+jest.mock("../component/AppBar", () => () => <div></div>);
+jest.mock("../component/PhotoSlider", () => () => <div></div>);
+jest.mock("../component/PitapatButton", () => () => <div></div>);
+
+const mockState = {
+  university: { universities: universities },
+  college: { colleges: colleges },
+  major: { majors: majors },
+  user: { users: users, loginUser: users[0] },
+  photo: { photos: photos },
+  tag: { tags: tags },
+  pitapat: { pitapats: pitapats },
+  chat: { chats: chats },
+};
+const mockStore = getMockStore(mockState);
+
+describe("ProfileDetail", () => {
+  function getElement(store: ToolkitStore) {
+    return (
+      <Provider store={store}>
+        <MemoryRouter initialEntries={["/profile/1"]}>
+          <Routes>
+            <Route path="/profile/:id" element={<ProfileDetail/>}/>
+          </Routes>
+        </MemoryRouter>
+      </Provider>
+    );
+  }
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("renders ProfileDetail", () => {
+    const { container } = render(getElement(mockStore));
+    expect(container).toBeTruthy();
+  });
+
+  it("redirects to SignIn page when not logged in", () => {
+    const logoutState = {
+      ...mockState,
+      user: { users: users, loginUser: null },
+    };
+    const mockLogoutStore = getMockStore(logoutState);
+    render(getElement(mockLogoutStore));
+    expect(mockNavigate).toBeCalled();
+  });
+
+  it("shows tags", () => {
+    render(getElement(mockStore));
+    users[0].tags.forEach((t) => {
+      let element: HTMLElement = document.createElement("div");
+      const tag = tags.find((tag) => tag.key === t);
+      if (tag) { element = screen.getByText(tag.name); }
+      expect(element).toBeInTheDocument();
+    });
+  });
+});
