e211bb17adc06705dc9a5f572a5726ca54461003
Kim Hyein <cozitive.dev@gmail.com>
2022-11-14 00:33:55 +0900

    Update: removed prefixes from model names

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -111,7 +111,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
-AUTH_USER_MODEL = 'pitapat.EUser'
+AUTH_USER_MODEL = 'pitapat.User'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
--- backend/pitapat/models.py
+++ backend/pitapat/models.py
@@ -3,76 +3,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
 from django.contrib.auth.hashers import make_password
 
-class ECollege(models.Model):
-    college_key = models.BigAutoField(primary_key=True)
-    college_name = models.CharField(max_length=20)
-    reg_dt = models.DateTimeField(auto_now_add=True)
-    reg_id = models.CharField(max_length=50)
-    upd_dt = models.DateTimeField(auto_now=True)
-    upd_id = models.CharField(max_length=50)
 
-    class Meta:
-        db_table = 'E_College'
-        verbose_name='College'
-
-
-class EIntroduction(models.Model):
-    introduction_key = models.BigAutoField(primary_key=True)
-    user_key = models.ForeignKey('EUser', models.DO_NOTHING, db_column='user_key')
-    field = models.TextField(db_column='Field')  # Field name made lowercase.
-    reg_dt = models.DateTimeField(auto_now_add=True)
-    reg_id = models.CharField(max_length=50)
-    upd_dt = models.DateTimeField(auto_now=True)
-    upd_id = models.CharField(max_length=50)
-
-    class Meta:
-        db_table = 'E_Introduction'
-        verbose_name='Introduction'
-
-
-class EMajor(models.Model):
-    major_key = models.BigAutoField(primary_key=True)
-    major_name = models.CharField(max_length=20)
-    reg_dt = models.DateTimeField(auto_now_add=True)
-    reg_id = models.CharField(max_length=50)
-    upd_dt = models.DateTimeField(auto_now=True)
-    upd_id = models.CharField(max_length=50)
-
-    class Meta:
-        db_table = 'E_Major'
-        verbose_name='Major'
-
-
-class EPhoto(models.Model):
-    photo_key = models.BigAutoField(primary_key=True)
-    user_key = models.ForeignKey('EUser', models.DO_NOTHING, db_column='user_key')
-    photo_name = models.CharField(max_length=50)
-    path = models.CharField(max_length=256)
-    reg_dt = models.DateTimeField(auto_now_add=True)
-    reg_id = models.CharField(max_length=50)
-    upd_dt = models.DateTimeField(auto_now=True)
-    upd_id = models.CharField(max_length=50)
-
-    class Meta:
-        db_table = 'E_Photo'
-        verbose_name='Photo'
-
-
-class ETag(models.Model):
-    tag_key = models.BigAutoField(primary_key=True)
-    tag_name = models.CharField(max_length=20)
-    tag_type = models.CharField(max_length=20)
-    reg_dt = models.DateTimeField(auto_now_add=True)
-    reg_id = models.CharField(max_length=50)
-    upd_dt = models.DateTimeField(auto_now=True)
-    upd_id = models.CharField(max_length=50)
-
-    class Meta:
-        db_table = 'E_Tag'
-        verbose_name='Tag'
-
-
-class EUniversity(models.Model):
+class University(models.Model):
     university_key = models.BigAutoField(primary_key=True)
     university_name = models.CharField(max_length=20)
     location = models.CharField(max_length=20)
@@ -88,7 +20,33 @@ class EUniversity(models.Model):
         verbose_name_plural='Universities'
 
 
-class EUserManager(UserManager):
+class College(models.Model):
+    college_key = models.BigAutoField(primary_key=True)
+    college_name = models.CharField(max_length=20)
+    reg_dt = models.DateTimeField(auto_now_add=True)
+    reg_id = models.CharField(max_length=50)
+    upd_dt = models.DateTimeField(auto_now=True)
+    upd_id = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'E_College'
+        verbose_name='College'
+
+
+class Major(models.Model):
+    major_key = models.BigAutoField(primary_key=True)
+    major_name = models.CharField(max_length=20)
+    reg_dt = models.DateTimeField(auto_now_add=True)
+    reg_id = models.CharField(max_length=50)
+    upd_dt = models.DateTimeField(auto_now=True)
+    upd_id = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'E_Major'
+        verbose_name='Major'
+
+
+class MyUserManager(UserManager):
     def _create_user(self, email, password, **extra_fields):
         if not email:
             raise ValueError("The given email must be set")
@@ -115,19 +73,19 @@ class EUserManager(UserManager):
         return self._create_user(
             email,
             password,
-            university_key=EUniversity.objects.get(university_name="admin"),
-            college_key=ECollege.objects.get(college_name="admin"),
-            major_key=EMajor.objects.get(major_name="admin"),
+            university_key=University.objects.get(university_name="admin"),
+            college_key=College.objects.get(college_name="admin"),
+            major_key=Major.objects.get(major_name="admin"),
             birthday=datetime.now(),
             **extra_fields,
         )
 
 
-class EUser(AbstractBaseUser, PermissionsMixin):
+class User(AbstractBaseUser, PermissionsMixin):
     user_key = models.BigAutoField(primary_key=True)
-    university_key = models.ForeignKey(EUniversity, models.DO_NOTHING, db_column='university_key')
-    college_key = models.ForeignKey(ECollege, models.DO_NOTHING, db_column='college_key')
-    major_key = models.ForeignKey(EMajor, models.DO_NOTHING, db_column='major_key')
+    university_key = models.ForeignKey(University, models.DO_NOTHING, db_column='university_key')
+    college_key = models.ForeignKey(College, models.DO_NOTHING, db_column='college_key')
+    major_key = models.ForeignKey(Major, models.DO_NOTHING, db_column='major_key')
     username = models.CharField(unique=True, max_length=30, db_column='user_name')
     email = models.CharField(unique=True, max_length=50)
     phone = models.CharField(unique=True, max_length=20)
@@ -144,16 +102,59 @@ class EUser(AbstractBaseUser, PermissionsMixin):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
 
-    objects = EUserManager()
+    objects = MyUserManager()
 
     class Meta:
         db_table = 'E_User'
         verbose_name='User'
 
 
-class RCollegemajor(models.Model):
-    college_key = models.OneToOneField(ECollege, models.DO_NOTHING, db_column='college_key', primary_key=True)
-    major_key = models.ForeignKey(EMajor, models.DO_NOTHING, db_column='major_key')
+class Introduction(models.Model):
+    introduction_key = models.BigAutoField(primary_key=True)
+    user_key = models.ForeignKey(User, models.DO_NOTHING, db_column='user_key')
+    field = models.TextField()
+    reg_dt = models.DateTimeField(auto_now_add=True)
+    reg_id = models.CharField(max_length=50)
+    upd_dt = models.DateTimeField(auto_now=True)
+    upd_id = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'E_Introduction'
+        verbose_name='Introduction'
+
+
+class Photo(models.Model):
+    photo_key = models.BigAutoField(primary_key=True)
+    user_key = models.ForeignKey(User, models.DO_NOTHING, db_column='user_key')
+    photo_name = models.CharField(max_length=50)
+    path = models.CharField(max_length=256)
+    reg_dt = models.DateTimeField(auto_now_add=True)
+    reg_id = models.CharField(max_length=50)
+    upd_dt = models.DateTimeField(auto_now=True)
+    upd_id = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'E_Photo'
+        verbose_name='Photo'
+
+
+class Tag(models.Model):
+    tag_key = models.BigAutoField(primary_key=True)
+    tag_name = models.CharField(max_length=20)
+    tag_type = models.CharField(max_length=20)
+    reg_dt = models.DateTimeField(auto_now_add=True)
+    reg_id = models.CharField(max_length=50)
+    upd_dt = models.DateTimeField(auto_now=True)
+    upd_id = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'E_Tag'
+        verbose_name='Tag'
+
+
+class CollegeMajor(models.Model):
+    college_key = models.OneToOneField(College, models.DO_NOTHING, db_column='college_key', primary_key=True)
+    major_key = models.ForeignKey(Major, models.DO_NOTHING, db_column='major_key')
 
     class Meta:
         db_table = 'R_CollegeMajor'
@@ -161,18 +162,18 @@ class RCollegemajor(models.Model):
         unique_together = (('college_key', 'major_key'),)
 
 
-class RPitapat(models.Model):
-    from_field = models.ForeignKey(EUser, models.DO_NOTHING, db_column='from', blank=True, null=True, related_name="from_field")  # Field renamed because it was a Python reserved word.
-    to = models.ForeignKey(EUser, models.DO_NOTHING, db_column='to', blank=True, null=True, related_name="to")
+class Pitapat(models.Model):
+    from_field = models.ForeignKey(User, models.DO_NOTHING, db_column='from', blank=True, null=True, related_name="from_field")  # Field renamed because it was a Python reserved word.
+    to = models.ForeignKey(User, models.DO_NOTHING, db_column='to', blank=True, null=True, related_name="to")
 
     class Meta:
         db_table = 'R_Pitapat'
         verbose_name='pitapat'
 
 
-class RUniversitycollege(models.Model):
-    university_key = models.OneToOneField(EUniversity, models.DO_NOTHING, db_column='university_key', primary_key=True)
-    college_key = models.ForeignKey(ECollege, models.DO_NOTHING, db_column='college_key')
+class UniversityCollege(models.Model):
+    university_key = models.OneToOneField(University, models.DO_NOTHING, db_column='university_key', primary_key=True)
+    college_key = models.ForeignKey(College, models.DO_NOTHING, db_column='college_key')
 
     class Meta:
         db_table = 'R_UniversityCollege'
@@ -180,9 +181,9 @@ class RUniversitycollege(models.Model):
         unique_together = (('university_key', 'college_key'),)
 
 
-class RUsertag(models.Model):
-    user_key = models.OneToOneField(EUser, models.DO_NOTHING, db_column='user_key', primary_key=True)
-    tag_key = models.ForeignKey(ETag, models.DO_NOTHING, db_column='tag_key')
+class UserTag(models.Model):
+    user_key = models.OneToOneField(User, models.DO_NOTHING, db_column='user_key', primary_key=True)
+    tag_key = models.ForeignKey(Tag, models.DO_NOTHING, db_column='tag_key')
 
     class Meta:
         db_table = 'R_UserTag'
