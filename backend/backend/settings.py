9356314fe08db6a7c28f527c889b284951fa7739
kl7407 <eee7407@naver.com>
2022-11-18 01:57:34 +0900

    Add: swagger

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'drf_yasg',
     'rest_framework',
     'rest_framework.authtoken',
     'dj_rest_auth',
--- backend/pitapat/urls.py
+++ backend/pitapat/urls.py
@@ -1,9 +1,32 @@
-from django.urls import include, path
+from django.urls import include, path, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 from . import views
 
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="pitapat campus api",
+        default_version="v0",
+        description="두근두근 캠퍼스 백엔드 api입니다",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(name="email", email="pitapatcampus@gmail.com"),
+        license=openapi.License(name="Test License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
+
 urlpatterns = [
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name="schema-json"),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('photo/', views.PhotoViewSet.as_view({'post': 'create'}), name='photo'),
+    path('tag/', views.TagViewSet.as_view({'get': 'list', 'post': 'create'}), name='tag'),
+    path('univ/', views.UniversityViewSet.as_view({'get': 'list'}), name='univ'),
     path('photo/', views.PhotoViewSet.as_view({
         'post': 'create',
     }), name='photo'),
