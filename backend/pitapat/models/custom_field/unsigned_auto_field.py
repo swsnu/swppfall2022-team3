e64b0a9ab6e70e9f09861f70dfccf321a0a146f9
Kim Hyein <cozitive.dev@gmail.com>
2022-11-17 14:51:39 +0900

    Fix: changed argument name

--- backend/pitapat/models/custom_field/unsigned_auto_field.py
+++ backend/pitapat/models/custom_field/unsigned_auto_field.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class UnsignedAutoField(models.BigAutoField):
-    def db_type(self, _conn):
+    def db_type(self, connection):
         return 'bigint UNSIGNED AUTO_INCREMENT'
 
-    def rel_db_type(self, _conn):
+    def rel_db_type(self, connection):
         return 'bigint UNSIGNED'
