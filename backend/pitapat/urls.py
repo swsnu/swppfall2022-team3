d0bee72c61438c20b634a0f131ea1d35a3459a84
kl7407 <eee7407@naver.com>
2022-11-19 02:22:33 +0900

    Update: url indentation

--- backend/pitapat/urls.py
+++ backend/pitapat/urls.py
@@ -20,53 +20,116 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name="schema-json"),
-    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('auth/', include('dj_rest_auth.urls')),
-    path('user/', views.UserViewSet.as_view({
-        'get': 'list',
-        'post': 'create',
-    }), name='user'),
-    path('user/<int:key>/', views.UserDetailViewSet.as_view({
-        'get': 'retrieve',
-        'put': 'partial_update',
-        'delete': 'destroy',
-    }), name='user_detail'),
-    path('universities/', views.UniversityViewSet.as_view({
-        'get': 'list',
-    }), name='universities'),
-    path('colleges/university/<int:university_key>/', views.CollegeViewSet.as_view({
-        'get': 'retrieve',
-    }), name='collegesuniv'),
-    path('majors/college/<int:college_key>/', views.MajorViewSet.as_view({
-        'get': 'retrieve',
-    }), name='majorscollege'),
-    path('photo/', views.PhotoViewSet.as_view({
-        'post': 'create',
-    }), name='photo'),
-    path('photo/<int:key>/', views.PhotoDetailViewSet.as_view({
-        'get': 'retrieve',
-        'delete': 'destroy',
-    }), name='photo_detail'),
-    path('tags/', views.TagViewSet.as_view({
-        'get': 'list',
-    }), name='tag'),
-    path('tags/user/<int:user_key>/', views.UserTagViewSet.as_view({
-        'get': 'list',
-        'post': 'create',
-        'delete': 'destroy',
-    }), name='user_tag'),
-    path('pitapat/', views.PitapatCreateViewSet.as_view({
-        'post': 'create',
-    }), name='pitapat_create'),
-    path('pitapat/<int:key>/', views.PitapatDeleteViewSet.as_view({
-        'delete': 'destroy',
-    }), name='pitapat_delete'),
-    path('pitapat/from/<int:user_key>', views.PitapatFromViewSet.as_view({
-        'get': 'retrieve',
-    }), name='pitapat_from'),
-    path('pitapat/to/<int:user_key>/', views.PitapatToViewSet.as_view({
-        'get': 'retrieve',
-    }), name='pitapat_to'),
+    re_path(
+        r'^swagger(?P<format>\.json|\.yaml)$',
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    re_path(
+        r'^swagger/$',
+        schema_view.with_ui('swagger', cache_timeout=0),
+        name='schema-swagger-ui',
+    ),
+    re_path(
+        r'^redoc/$',
+        schema_view.with_ui('redoc', cache_timeout=0),
+        name='schema-redoc',
+    ),
+    path(
+        'auth/',
+        include('dj_rest_auth.urls'),
+    ),
+    path(
+        'user/',
+        views.UserViewSet.as_view({
+            'get': 'list',
+            'post': 'create',
+        }),
+        name='user',
+    ),
+    path(
+        'user/<int:key>/',
+        views.UserDetailViewSet.as_view({
+            'get': 'retrieve',
+            'put': 'partial_update',
+            'delete': 'destroy',
+        }),
+        name='user_detail',
+    ),
+    path(
+        'universities/',
+        views.UniversityViewSet.as_view({
+            'get': 'list',
+        }),
+        name='universities',
+    ),
+    path(
+        'colleges/university/<int:university_key>/',
+        views.CollegeViewSet.as_view({
+            'get': 'retrieve',
+        }),
+        name='collegesuniv',
+    ),
+    path(
+        'majors/college/<int:college_key>/',
+        views.MajorViewSet.as_view({
+            'get': 'retrieve',
+        }),
+        name='majorscollege',
+    ),
+    path(
+        'photo/',
+        views.PhotoViewSet.as_view({
+            'post': 'create',
+        }),
+        name='photo',
+    ),
+    path(
+        'photo/<int:key>/',
+        views.PhotoDetailViewSet.as_view({
+            'get': 'retrieve',
+            'delete': 'destroy',
+        }),
+        name='photo_detail',
+    ),
+    path(
+        'tags/',
+        views.TagViewSet.as_view({
+            'get': 'list',
+        }),
+        name='tag',
+    ),
+    path(
+        'tags/user/<int:user_key>/',
+        views.UserTagViewSet.as_view({
+            'get': 'list',
+            'post': 'create',
+            'delete': 'destroy',
+        }),
+        name='user_tag',
+    ),
+    path(
+        'pitapat/', views.PitapatCreateViewSet.as_view({
+            'post': 'create',
+        }),
+        name='pitapat_create',
+    ),
+    path(
+        'pitapat/<int:key>/', views.PitapatDeleteViewSet.as_view({
+            'delete': 'destroy',
+        }),
+        name='pitapat_delete',
+    ),
+    path(
+        'pitapat/from/<int:user_key>', views.PitapatFromViewSet.as_view({
+            'get': 'retrieve',
+        }),
+        name='pitapat_from',
+    ),
+    path(
+        'pitapat/to/<int:user_key>/', views.PitapatToViewSet.as_view({
+            'get': 'retrieve',
+        }),
+        name='pitapat_to',
+    ),
 ]
