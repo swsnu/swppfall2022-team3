912fc348a1d49f9da1a1d2459c6f62883011ae73
Kim Hyein <cozitive.dev@gmail.com>
2022-11-20 16:49:08 +0900

    Update: add last chat to chatroom

--- backend/pitapat/serializers/chatroom.py
+++ backend/pitapat/serializers/chatroom.py
@@ -2,7 +2,7 @@ from django.db.models import Q
 from rest_framework import serializers
 
 from backend.settings import IMAGE_URL
-from pitapat.models import Chatroom, User, UserChatroom
+from pitapat.models import Chatroom, Chat, UserChatroom
 
 
 class UserChatroomSerializer(serializers.ModelSerializer):
@@ -23,6 +23,14 @@ class UserChatroomSerializer(serializers.ModelSerializer):
         return f'{IMAGE_URL}{user.photos.all()[0].name}' if user.photos.all() else ''
     image_path = serializers.SerializerMethodField()
 
+    def get_last_chat(self, user_chatroom: UserChatroom):
+        chatroom = user_chatroom.chatroom
+        chats = chatroom.chats.all()
+        if not chats:
+            return ''
+        return chats.order_by('-reg_dt')[0].content
+    last_chat = serializers.SerializerMethodField()
+
     class Meta:
         model = UserChatroom
-        fields = ['chatroom', 'name', 'image_path']
+        fields = ['chatroom', 'name', 'image_path', 'last_chat']
