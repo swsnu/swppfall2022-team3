d100dec93e619bbf86c7c207a5dd05f2f47bf1f1
pacifiic <yellowbill@snu.ac.kr>
2022-11-20 17:25:19 +0900

    Update: Major test

--- frontend/src/store/slices/major.test.ts
+++ frontend/src/store/slices/major.test.ts
@@ -1,10 +1,13 @@
 import { AnyAction, configureStore, EnhancedStore, ThunkMiddleware } from "@reduxjs/toolkit";
-import { majors } from "../../dummyData";
+import axios from "axios";
+import {colleges, majors} from "../../dummyData";
 import { Major } from "../../types";
 import majorReducer from "./major";
+import { getMajors } from "./major";
 
 
 describe("major reducer", () => {
+  const testMajor = majors[0];
   let store: EnhancedStore<
     { major: { majors: Major[] } },
     AnyAction,
@@ -16,6 +19,15 @@ describe("major reducer", () => {
   });
 
   it("should have initial state", () => {
-    expect(store.getState().major.majors).toEqual(majors);
+    expect(store.getState().major.majors).toEqual([]);
+  });it("should handle getColleges", async () => {
+    axios.get = jest.fn().mockResolvedValue({ data: [testMajor], status: 200 });
+    await store.dispatch(getMajors(3));
+    expect(store.getState().major.majors).toEqual([testMajor]);
+  });
+  it("should handle 500 getColleges", async () => {
+    axios.get = jest.fn().mockResolvedValue({ data: [], status: 500 });
+    await store.dispatch(getMajors(3));
+    expect(store.getState().major.majors).toEqual([]);
   });
 });
