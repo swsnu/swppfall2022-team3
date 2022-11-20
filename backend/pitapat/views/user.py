eea9f30c6e445be0de718ff0e1516328a7427f78
Kim Hyein <cozitive.dev@gmail.com>
2022-11-20 15:08:01 +0900

    Update: handle bad request to UserIntroductionViewSet

--- backend/pitapat/views/user.py
+++ backend/pitapat/views/user.py
@@ -1,16 +1,14 @@
 from datetime import datetime
 
 from django.db.models import Count, Q
-from django.http import HttpResponseBadRequest
 from django.shortcuts import get_object_or_404
 from drf_yasg.utils import swagger_auto_schema
-from rest_framework import serializers
-from rest_framework import viewsets
+from rest_framework import serializers, viewsets
 from rest_framework.response import Response
 
 from pitapat.models import Introduction, Photo, User, UserTag, Tag
-from pitapat.serializers import UserListSerializer, UserListFilterSerializer, UserCreateSerializer, \
-    UserDetailSerializer, UserIntroductionSerializer
+from pitapat.serializers import (UserListSerializer, UserListFilterSerializer, UserCreateSerializer,
+                                 UserDetailSerializer, UserIntroductionSerializer)
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -117,7 +115,7 @@ class UserDetailViewSet(viewsets.ModelViewSet):
 class UserIntroductionViewSet(viewsets.ModelViewSet):
     http_method_names = ['get', 'post', 'put']
     queryset = Introduction.objects.all()
-    serializer_class = str
+    # TODO: set serializer_class
 
     def retrieve(self, request, *args, **kwargs):
         user = get_object_or_404(User.objects.all(), key=kwargs['user_key'])
@@ -125,22 +123,20 @@ class UserIntroductionViewSet(viewsets.ModelViewSet):
         return Response(introduction.content)
 
     @swagger_auto_schema(request_body=UserIntroductionSerializer)
-    def update(self, request, *args, **kwargs):
+    def create(self, request, *args, **kwargs):
         user = get_object_or_404(User.objects.all(), key=kwargs['user_key'])
-        introduction = get_object_or_404(Introduction.objects.all(), user=user)
-        content = request.data['content']
-        if content is None:
-            return HttpResponseBadRequest()
-        introduction.content = content
-        introduction.save()
+        content = request.data.get('content')
+        if content is None or Introduction.objects.filter(user=user).count() != 0:
+            return Response(status=404)
+        introduction = Introduction.objects.create(user=user, content=content)
         return Response(introduction.content)
 
     @swagger_auto_schema(request_body=UserIntroductionSerializer)
-    def create(self, request, *args, **kwargs):
+    def update(self, request, *args, **kwargs):
         user = get_object_or_404(User.objects.all(), key=kwargs['user_key'])
-        content = request.data['content']
-        if content is None or Introduction.objects.filter(user=user).count() != 0:
-            return HttpResponseBadRequest()
-        introduction = Introduction(user=user, content=content)
-        introduction.save()
+        introduction = get_object_or_404(Introduction.objects.all(), user=user)
+        content = request.data.get('content')
+        if content is None:
+            return Response(status=404)
+        introduction = Introduction.objects.create(user=user, content=content)
         return Response(introduction.content)
