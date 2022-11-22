4f7a508b80e452110cd840e636827ab6f03132c0
Kim Hyein <cozitive.dev@gmail.com>
2022-11-22 15:43:16 +0900

    Update: async consumer

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -13,7 +13,8 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-import pitapat.routing
+
+from pitapat.routing import websocket_urlpatterns
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
@@ -23,7 +24,7 @@ application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
         "websocket": AllowedHostsOriginValidator(
-            AuthMiddlewareStack(URLRouter(pitapat.routing.websocket_urlpatterns))
+            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
         ),
     }
 )
--- backend/pitapat/consumers.py
+++ backend/pitapat/consumers.py
@@ -1,41 +1,42 @@
 import json
 
 from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+from channels.db import database_sync_to_async
+from channels.generic.websocket import AsyncWebsocketConsumer
 # from pitapat.models import User, Chatroom, Chat
 
 
-class ChatConsumer(WebsocketConsumer):
+class ChatConsumer(AsyncWebsocketConsumer):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         # self.chatroom_key = self.scope['url_route']['kwargs']['chatroom_key']
         # self.room_group_name = f'chatroom_{self.chatroom_key}'
         # self.chatroom = Chatroom.objects.get(key=self.chatroom_key)
 
-    def connect(self):
+    async def connect(self):
         self.chatroom_key = self.scope['url_route']['kwargs']['chatroom_key']
         self.room_group_name = f'chatroom_{self.chatroom_key}'
 
-        async_to_sync(self.channel_layer.group_add)(
+        await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name,
         )
-        self.accept()
+        await self.accept()
 
-    def disconnect(self, code):
-        async_to_sync(self.channel_layer.group_discard)(
+    async def disconnect(self, code):
+        await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name,
         )
 
-    def receive(self, text_data=None, bytes_data=None):
+    async def receive(self, text_data=None, bytes_data=None):
         text_data_json: dict = json.loads(text_data)
         # method_type = text_data_json['type']
         content = text_data_json.setdefault('message', None)
         # author_key = text_data_json.setdefault('author', None)
         # chat_key = text_data_json.setdefault('chat', None)
 
-        async_to_sync(self.channel_layer.group_send)(
+        await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
@@ -81,7 +82,8 @@ class ChatConsumer(WebsocketConsumer):
         #     # invalid method type,
         #     return
 
-    def chat_message(self, event):
-        print(event)
+    async def chat_message(self, event):
         content = event['content']
-        self.send(text_data=json.dumps({'message': content}))
+        await self.send(text_data=json.dumps({
+            'message': content,
+        }))
