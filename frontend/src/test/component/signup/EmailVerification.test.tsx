b206d975b4e16a635d875ba5997b767f3f0de0eb
kl7407 <eee7407@naver.com>
2022-11-25 17:03:19 +0900

    Update: EmailVerification test

--- frontend/src/test/component/signup/EmailVerification.test.tsx
+++ frontend/src/test/component/signup/EmailVerification.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { act, fireEvent, render, screen, waitFor } from "@testing-library/react";
 import axios from "axios";
 import EmailVerification from "../../../component/signup/EmailVerification";
 
@@ -29,6 +29,25 @@ describe("EmailVerification", () => {
     expect(screen.getByText("재전송")).toBeInTheDocument();
   });
 
+  it("should also be rendered.. for higher coverage", () => {
+    jest.useFakeTimers();
+    render(
+      <EmailVerification
+        email={""}
+        limitSec={59}
+        requestTime={new Date()}
+        setRequestTime={mockSetRequestTime}
+        setStep={mockSetStep}
+      />
+    );
+
+    act(() => {
+      jest.advanceTimersByTime(60 * 1000 + 1);
+    });
+
+    expect(screen.getByText("재전송")).toBeInTheDocument();
+  });
+
   it("should call setStep with status code 204", async () => {
     axios.post = jest.fn().mockResolvedValue({ status: 204 });
     render(component());
@@ -36,6 +55,16 @@ describe("EmailVerification", () => {
     fireEvent.click(confirmButton);
     await waitFor(() => {
       expect(mockSetStep).toBeCalled();
+    });
+  });
+
+  it("should not call setStep when the status code is not 204", async () => {
+    axios.post = jest.fn().mockResolvedValue({ status: 500 });
+    render(component());
+    const confirmButton = screen.getByText("확인");
+    fireEvent.click(confirmButton);
+    await waitFor(() => {
+      expect(mockSetStep).not.toBeCalled();
     });
   });
 
@@ -48,6 +77,27 @@ describe("EmailVerification", () => {
     await waitFor(() => {
       expect(window.alert).toBeCalled();
     });
+  });
+
+  it ("should alert when the time is over", async () => {
+    const spyAlert = jest.spyOn(window, "alert").mockImplementation(() => true);
+    axios.post = jest.fn().mockResolvedValue({ status: 204 });
+    jest.useFakeTimers();
+
+    render(
+      <EmailVerification
+        email={""}
+        limitSec={0}
+        requestTime={new Date()}
+        setRequestTime={mockSetRequestTime}
+        setStep={mockSetStep}
+      />
+    );
+
+    act(() => {
+      jest.advanceTimersByTime(3 * 60 * 1000 + 1);
+    });
+    expect(spyAlert).toBeCalled();
   });
 
   it("should handle resend button click", async () => {
