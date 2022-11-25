2ab64daedc14b7178255369d92446608d711de8c
Kim Hyein <cozitive.dev@gmail.com>
2022-11-25 21:22:48 +0900

    Remove: unused methods

--- backend/pitapat/views/user_introduction.py
+++ backend/pitapat/views/user_introduction.py
@@ -12,24 +12,6 @@ class UserIntroductionViewSet(viewsets.ModelViewSet):
     queryset = Introduction.objects.all()
     serializer_class = IntroductionSerializer
 
-    '''
-    def retrieve(self, request, *args, **kwargs):
-        user = get_object_or_404(User.objects.all(), key=kwargs['user_key'])
-        introduction = get_object_or_404(Introduction.objects.all(), user=user)
-        serializer = IntroductionSerializer(introduction)
-        return Response(serializer.data)
-
-    @swagger_auto_schema(request_body=IntroductionSerializer)
-    def create(self, request, *args, **kwargs):
-        user = get_object_or_404(User.objects.all(), key=kwargs['user_key'])
-        content = request.data.get('content')
-        if content is None or Introduction.objects.filter(user=user).count() != 0:
-            return Response(status=404)
-        introduction = Introduction.objects.create(user=user, content=content)
-        serializer = IntroductionSerializer(introduction)
-        return Response(serializer.data)
-    '''
-
     @swagger_auto_schema(request_body=IntroductionSerializer)
     def update(self, request, *args, **kwargs):
         user = get_object_or_404(User.objects.all(), key=kwargs['user_key'])
