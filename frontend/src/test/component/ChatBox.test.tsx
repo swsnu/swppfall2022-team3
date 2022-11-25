4edb6bb5923c4b8b9fdf5a5320417385b51689bf
pacifiic <yellowbill@snu.ac.kr>
2022-11-25 12:46:27 +0900

    Update: ChatBox test

--- frontend/src/test/component/ChatBox.test.tsx
+++ frontend/src/test/component/ChatBox.test.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { fireEvent, render, screen } from "@testing-library/react";
-import ChatBox from "../../component/ChatBox";
+import ChatBox, { IProps } from "../../component/ChatBox";
 import { users } from "../../dummyData";
 import { getDefaultMockStore } from "../../test-utils/mocks";
 
@@ -12,52 +12,52 @@ jest.mock("react-router-dom", () => ({
   useNavigate: () => mockNavigate,
 }));
 
+const mockIPropsReceived: IProps = {
+  sender: users[3],
+  content: "hi",
+};
+
+const mockIPropsSent: IProps = {
+  sender: users[0],
+  content: "hi",
+};
+
 const mockStore = getDefaultMockStore();
 
 describe("ChatBox", () => {
-  it("should be rendered", () => {
-    const chatContent = "test chat from me";
-    render(
+
+  function getElement(props: IProps) {
+    return (
       <Provider store={mockStore}>
         <ChatBox
-          content={chatContent}
-          sender={users[0]}
+          content={props.content}
+          sender={props.sender}
         />
       </Provider>
     );
+  }
+  it("should be rendered when received", () => {
+    render(getElement(mockIPropsReceived));
+    const chatBox = screen.getByText(mockIPropsReceived.content);
+    expect(chatBox).toBeInTheDocument();
+  });
 
-    const chatBox = screen.getByText(chatContent);
-
+  it("should be rendered when sent", () => {
+    render(getElement(mockIPropsSent));
+    const chatBox = screen.getByText(mockIPropsSent.content);
     expect(chatBox).toBeInTheDocument();
   });
 
   it("should have image when it is not mine", () => {
-    const chatContent = "test chat from the other";
-    render(
-      <ChatBox
-        content={chatContent}
-        sender={users[1]}
-      />
-    );
-
+    render(getElement(mockIPropsReceived));
     const image = screen.getByRole("img");
-
     expect(image).toBeInTheDocument();
   });
 
   it("should navigate to other user's detail page when image is clicked", () => {
-    const chatContent = "test chat from the other";
-    render(
-      <ChatBox
-        content={chatContent}
-        sender={users[1]}
-      />
-    );
-
+    render(getElement(mockIPropsReceived));
     const image = screen.getByRole("img");
-
     fireEvent.click(image);
-
     expect(mockNavigate).toBeCalled();
   });
 });
