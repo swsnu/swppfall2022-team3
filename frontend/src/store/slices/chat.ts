9819dd4b052dea055f01c5d09520b8a543537982
pacifiic <yellowbill@snu.ac.kr>
2022-12-03 14:59:05 +0900

    Remove: minor unnecessary line

--- frontend/src/store/slices/chat.ts
+++ frontend/src/store/slices/chat.ts
@@ -3,7 +3,6 @@ import axios from "axios";
 import { Chat, Chatroom, User } from "../../types";
 import { dateToString } from "../../util/date";
 import { RootState } from "../index";
-import { SimplifiedRawUser, simplifiedRawDataToUser } from "./user";
 
 
 export const userUrl = "/user";
