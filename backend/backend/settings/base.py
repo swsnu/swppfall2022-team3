03023b886d7e3fb3f7b8cd14ee9a32b653916c64
kl7407 <eee7407@naver.com>
2022-12-04 21:16:21 +0900

    Update: settings for static files

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -162,6 +162,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATIC_ROOT = f'{BASE_DIR}/static'
 STATIC_URL = 'static/'
 
 # Default primary key field type
