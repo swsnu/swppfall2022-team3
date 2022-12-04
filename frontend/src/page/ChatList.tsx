a57db77aade3741ec1d90c77ae2407753315b0eb
pacifiic <yellowbill@snu.ac.kr>
2022-12-04 18:45:39 +0900

    Update: ChatList save Y Position

--- frontend/src/page/ChatList.tsx
+++ frontend/src/page/ChatList.tsx
@@ -1,5 +1,6 @@
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useLocation } from "react-router";
 import { Navigate } from "react-router-dom";
 import AppBar from "../component/AppBar";
 import ChatListElement from "../component/ChatListElement";
@@ -9,12 +10,23 @@ import style from "../constant/style";
 import { AppDispatch } from "../store";
 import { getChatrooms, selectChat } from "../store/slices/chat";
 import { selectUser } from "../store/slices/user";
+import { savePageYPosition, scrollToPrevPosition } from "../util/pageScroll";
 
 
 export default function ChatList() {
   const loginUser = useSelector(selectUser).loginUser;
   const chatrooms = useSelector(selectChat).chatrooms;
   const dispatch = useDispatch<AppDispatch>();
+  const urlPath = useLocation().pathname;
+  const pageBody = useRef<HTMLDivElement>(null);
+
+  const saveYPosition = useCallback(() => {
+    savePageYPosition(pageBody, urlPath);
+  }, [pageBody, urlPath]);
+
+  useEffect(() => {
+    scrollToPrevPosition(pageBody, urlPath);
+  }, [pageBody, urlPath]);
 
   useEffect(() => {
     if (loginUser) {
@@ -26,17 +38,23 @@ export default function ChatList() {
     loginUser ?
       <section className={`${style.page.base} ${style.page.margin.top} ${style.page.margin.bottom}`}>
         <AppBar/>
-        <section className={style.page.body}>{
-          chatrooms.map(({ key, name, imagePath, chats }) => (
-            <ChatListElement
-              key={key}
-              chatroomKey={key}
-              chatroomName={name}
-              imagePath={imagePath}
-              lastChat={chats.length === 0 ? null : chats[chats.length - 1].content}
-            />
-          ))
-        }</section>
+        <section
+          className={style.page.body}
+          role={"presentation"}
+          ref={pageBody}
+          onClick={saveYPosition}
+        >
+          {
+            chatrooms.map(({ key, name, imagePath, chats }) => (
+              <ChatListElement
+                key={key}
+                chatroomKey={key}
+                chatroomName={name}
+                imagePath={imagePath}
+                lastChat={chats.length === 0 ? null : chats[chats.length - 1].content}
+              />
+            ))
+          }</section>
         <NavigationBar/>
       </section> :
       <Navigate replace to={paths.signIn}/>
