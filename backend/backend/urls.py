9403fd007440daf43eca43993e950a68a3e1efdc
Kim Hyein <cozitive.dev@gmail.com>
2022-12-05 15:40:29 +0900

    Fix: backend root url

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pitapat.urls')),
+    path('api/', include('pitapat.urls')),
 ]
