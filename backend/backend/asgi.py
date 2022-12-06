f0d1bd9987d61f9d07b48d2f294198d3382d8044
kl7407 <eee7407@naver.com>
2022-12-06 20:41:35 +0900

    Update: asgi

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -14,11 +14,13 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings.local")
+django_asgi_app = get_asgi_application()
+
+
 from pitapat.routing import websocket_urlpatterns
 
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter(
     {
