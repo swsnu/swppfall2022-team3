1c630f4d86ed24d4ad9cc318ddace09d4bdbddd0
kl7407 <eee7407@naver.com>
2022-12-08 17:49:49 +0900

    Update: chat reducer test

--- frontend/src/test/store/slices/chat.test.ts
+++ frontend/src/test/store/slices/chat.test.ts
@@ -1,7 +1,7 @@
 import { AnyAction, configureStore, EnhancedStore, ThunkMiddleware } from "@reduxjs/toolkit";
 import axios from "axios";
 import { chatrooms } from "../../../dummyData";
-import chatReducer, { RawChatroom, getChatrooms, ChatState } from "../../../store/slices/chat";
+import chatReducer, { RawChatroom, getChatrooms, ChatState, chatAction } from "../../../store/slices/chat";
 // import { getUsers } from "../../../store/slices/user";
 import { Chatroom } from "../../../types";
 
@@ -34,13 +34,45 @@ describe("chat reducer", () => {
     expect(store.getState().chat.chatrooms).toEqual([]);
   });
 
-  it("should get chatrooms", async () => {
-    axios.get = jest.fn().mockResolvedValue({ data: [testRawChatroom], status: 200 });
+  it("should set chatroom empty", () => {
+    store.dispatch(chatAction.setChatroomEmpty());
+    expect(store.getState().chat.chatrooms).toEqual([]);
+    expect(store.getState().chat.chatSockets).toEqual([]);
+  });
+
+  it("should get chatrooms and set chats properly", async () => {
+    const noChatChatroom = chatroomToRawChatroom({
+      key: 2,
+      name: "두 번째 채팅방",
+      imagePath: "",
+      chats: [],
+    });
+    axios.get = jest.fn().mockResolvedValue({
+      data: [
+        testRawChatroom,
+        noChatChatroom,
+      ],
+      status: 200
+    });
 
     await store.dispatch(getChatrooms(1));
-    expect(store.getState().chat.chatrooms.map((r) => r.key)).toEqual([testChatroom.key]);
+    expect(store.getState().chat.chatrooms.map((r) => r.key))
+      .toEqual([testChatroom.key, noChatChatroom.chatroom]);
+    // this is for the coverage. nothing should be changed
+    await store.dispatch(getChatrooms(1));
+    expect(store.getState().chat.chatrooms.map((r) => r.key))
+      .toEqual([testChatroom.key, noChatChatroom.chatroom]);
     expect(store.getState().chat.chatrooms[0].chats[0].content)
       .toEqual(testChatroom.chats[testChatroom.chats.length - 1].content);
+
+
+    store.dispatch(chatAction.setChatroomChats({ chatroomKey: testChatroom.key, chats: testChatroom.chats }));
+    store.dispatch(chatAction.addChat(testChatroom.chats[0]));
+    // this is for the coverage exceptional case
+    store.dispatch(chatAction.setChatroomChats({ chatroomKey: -1, chats: [] }));
+    // this is for the coverage exceptional case
+    store.dispatch(chatAction.addChat({ ...testChatroom.chats[0], chatroomKey: -1 }));
+    expect(store.getState().chat.chatrooms[0].chats.length).toEqual(testChatroom.chats.length + 1);
   });
 
   it("should not get chatrooms", async () => {
