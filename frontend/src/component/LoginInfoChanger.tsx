196426ab4898a3f5be60d38a6d61047a1fe1e810
Kim Hyein <cozitive.dev@gmail.com>
2022-12-13 17:04:06 +0900

    Update: edit components

--- frontend/src/component/LoginInfoChanger.tsx
+++ frontend/src/component/LoginInfoChanger.tsx
@@ -5,6 +5,7 @@ import style from "../constant/style";
 import { AppDispatch } from "../store";
 import { fetchSignin, selectUser } from "../store/slices/user";
 import { passwordChange, userUrl } from "../store/urls";
+import AlertModal from "./AlertModal";
 import InformationInput from "./signup/InformationInput";
 
 
@@ -18,46 +19,50 @@ export default function LoginInfoChanger({ onModalClose }: IProps) {
   const [nickname, setNickname] = useState<string>(loginUser?.nickname ?? "");
   const [password, setPassword] = useState<string>("");
   const [passwordCheck, setPasswordCheck] = useState<string>("");
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const [modalMessage, setModalMessage] = useState<JSX.Element>(<div/>);
 
   const confirmOnClick = useCallback(async () => {
     if (password === "" || nickname === "") {
-      alert("닉네임과 비밀번호를 입력해야 합니다.");
+      setModalMessage(<p>닉네임과 비밀번호를<br/>입력해야 합니다.</p>);
+      setModalOpen(true);
+      return;
     }
-    else if (password !== passwordCheck) {
-      alert("비밀번호가 일치하지 않습니다.");
+    if (password !== passwordCheck) {
+      setModalMessage(<p>비밀번호가 일치하지 않습니다.</p>);
+      setModalOpen(true);
+      return;
     }
-    else {
-      const newLoginUser = {
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        username: loginUser!.email,
-        password: password,
-      };
-      try {
-        await axios.post(`${passwordChange}/`, {
-          new_password1: password,
-          new_password2: password,
-        });
-      }
-      catch {
-        alert("비밀번호가 너무 쉽습니다.");
-        onModalClose();
-        return;
-      }
-      await axios.put(`${userUrl}/${loginUser?.key}/`, {
-        email: loginUser?.email,
-        nickname: nickname,
-        gender: loginUser?.gender,
-        interested_gender: loginUser?.interestedGender,
-        birthday: loginUser?.birthday,
-        university: loginUser?.university,
-        college: loginUser?.college,
-        major: loginUser?.major,
-        introduction: loginUser?.introduction,
-        tags: loginUser?.tags,
+    const newLoginUser = {
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      username: loginUser!.email,
+      password: password,
+    };
+    try {
+      await axios.post(`${passwordChange}/`, {
+        new_password1: password,
+        new_password2: password,
       });
-      dispatch(fetchSignin(newLoginUser));
-      onModalClose();
     }
+    catch {
+      setModalMessage(<p>비밀번호가 너무 쉽습니다.</p>);
+      setModalOpen(true);
+      return;
+    }
+    await axios.put(`${userUrl}/${loginUser?.key}/`, {
+      email: loginUser?.email,
+      nickname: nickname,
+      gender: loginUser?.gender,
+      interested_gender: loginUser?.interestedGender,
+      birthday: loginUser?.birthday,
+      university: loginUser?.university,
+      college: loginUser?.college,
+      major: loginUser?.major,
+      introduction: loginUser?.introduction,
+      tags: loginUser?.tags,
+    });
+    dispatch(fetchSignin(newLoginUser));
+    onModalClose();
   }, [
     dispatch,
     loginUser,
@@ -73,45 +78,52 @@ export default function LoginInfoChanger({ onModalClose }: IProps) {
   [onModalClose]);
 
   return (
-    <section className={"h-fit w-fit flex flex-col items-center bg-white p-4"}>
-      <section className={"space-y-4"}>
-        <InformationInput
-          label={"닉네임"}
-          value={nickname}
-          setValue={setNickname}
-          type={"text"}
-          required={true}
-        />
-        <InformationInput
-          label={"비밀번호"}
-          value={password}
-          setValue={setPassword}
-          type={"text"}
-          required={true}
-          isPassword={true}
-        />
-        <InformationInput
-          label={"비밀번호 확인"}
-          value={passwordCheck}
-          setValue={setPasswordCheck}
-          type={"text"}
-          required={true}
-          isPassword={true}
-        />
-      </section>
-      <section className={style.component.signIn.buttonWrapper}>
-        <button
-          className={`${style.button.base} ${style.button.colorSet.main}`}
-          onClick={confirmOnClick}
-        >
+    <section>
+      <AlertModal
+        description={modalMessage}
+        modalOpen={modalOpen}
+        setModalOpen={setModalOpen}
+      />
+      <section className={"h-fit w-fit flex flex-col items-center bg-white p-4"}>
+        <article className={"space-y-4"}>
+          <InformationInput
+            label={"닉네임"}
+            value={nickname}
+            setValue={setNickname}
+            type={"text"}
+            required={true}
+          />
+          <InformationInput
+            label={"비밀번호"}
+            value={password}
+            setValue={setPassword}
+            type={"text"}
+            required={true}
+            isPassword={true}
+          />
+          <InformationInput
+            label={"비밀번호 확인"}
+            value={passwordCheck}
+            setValue={setPasswordCheck}
+            type={"text"}
+            required={true}
+            isPassword={true}
+          />
+        </article>
+        <article className={"flex flex-col mt-8"}>
+          <button
+            className={`${style.button.base} ${style.button.colorSet.main}`}
+            onClick={confirmOnClick}
+          >
           적용
-        </button>
-        <button
-          className={`${style.button.base} ${style.button.colorSet.secondary} mt-8`}
-          onClick={cancelOnClick}
-        >
+          </button>
+          <button
+            className={`${style.button.base} ${style.button.colorSet.secondary} mt-4`}
+            onClick={cancelOnClick}
+          >
           취소
-        </button>
+          </button>
+        </article>
       </section>
     </section>
   );
--- frontend/src/component/profile-edit/TagsEdit.tsx
+++ frontend/src/component/profile-edit/TagsEdit.tsx
@@ -11,6 +11,7 @@ import { selectTag } from "../../store/slices/tag";
 import { getLoginUser, selectUser } from "../../store/slices/user";
 import { userUrl } from "../../store/urls";
 import { Tag } from "../../types";
+import AlertModal from "../AlertModal";
 import TagElement from "../signup/TagElement";
 
 
@@ -25,25 +26,27 @@ export default function TagsEdit({ onModalClose }: IProps) {
   const [newTags, setNewTags] = useState<Tag[]>([]);
   const [selectedTag, setSelectedTag] = useState<Tag>();
   const [selectedTagKey, setSelectedTagKey] = useState<number>(1);
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const [modalMessage, setModalMessage] = useState<JSX.Element>(<div/>);
 
-
-  const cancelOnClickHandler = useCallback( () => {
+  const cancelOnClick = useCallback( () => {
     onModalClose();
   },
   [onModalClose]);
 
-  const confirmHandler = useCallback( async () => {
+  const confirmOnClick = useCallback( async () => {
     if (!loginUser) {
       return;
     }
-    if (newTags.length !== 0) {
-      await axios.delete(`${userUrl}/${loginUser.key}${paths.tag}/`);
-      await axios.post(`${userUrl}/${loginUser.key}${paths.tag}/`, { tags: newTags.map((tag) => tag.key) });
-      dispatch(getLoginUser(loginUser.key));
+    if (newTags.length === 0) {
+      setModalMessage(<p>최소한 한 개의 태그가 있어야 해요.</p>);
+      setModalOpen(true);
+      return;
     }
-    else {
-      alert("최소한 한 개의 태그가 있어야 해요.");
-    }
+
+    await axios.delete(`${userUrl}/${loginUser.key}${paths.tag}/`);
+    await axios.post(`${userUrl}/${loginUser.key}${paths.tag}/`, { tags: newTags.map((tag) => tag.key) });
+    await dispatch(getLoginUser(loginUser.key));
     onModalClose();
   },
   [dispatch, loginUser, onModalClose, newTags]);
@@ -67,78 +70,83 @@ export default function TagsEdit({ onModalClose }: IProps) {
   }, [newTags, setNewTags]);
 
   return (
-    <section className={"h-fit w-fit flex flex-col items-center bg-white p-4 space-y-8"}>
-      <section className={"space-y-4"}>
-        <div>
-          수정할 태그를 선택해 주세요.
-        </div>
-        <section className={"w-full flex-1 flex flex-col items-center"}>
-          <div className={"flex flex-row justify-center mt-8"}>
-            <article className={"flex flex-col item-center"}>
-              <FormControl
-                sx={{
-                  maxWidth: 320,
-                  minWidth: 240,
-                }}
-                size={"small"}
-                required
-              >
-                <InputLabel>
-                  {"태그"}
-                </InputLabel>
-                <Select
-                  label={"태그"}
-                  variant={"outlined"}
-                  value={selectedTagKey}
-                  onChange={(e) => {
-                    (setSelectedTagKey as Dispatch<SetStateAction<string | number>>)(e.target.value);
+    <section>
+      <AlertModal
+        description={modalMessage}
+        modalOpen={modalOpen}
+        setModalOpen={setModalOpen}
+      />
+      <section className={"h-fit w-80 max-w-xs max flex flex-col items-center bg-white p-4 space-y-8"}>
+        <section className={"space-y-4"}>
+          <p>수정할 태그를 선택해 주세요.</p>
+          <section className={"w-full flex-1 flex flex-col"}>
+            <div className={"flex flex-row justify-center mt-2"}>
+              <article className={"flex flex-col item-center"}>
+                <FormControl
+                  sx={{
+                    maxWidth: 320,
+                    minWidth: 240,
                   }}
+                  size={"small"}
+                  required
                 >
-                  {
-                    loadedTags
-                      .map(({ name, key }) => (<MenuItem value={key} key={key}>{name}</MenuItem>))
-                  }
-                </Select>
-              </FormControl>
-            </article>
-            <button
-              className={"add-tag-button ml-2"}
-              onClick={addTagOnClick}
-            >
-              <AddCircleIcon
-                style={{ color: pink[500] }}
-                fontSize={"large"}
-              />
-            </button>
-          </div>
-          <article
-            className={"w-4/5 max-w-xs flex flex-row flex-wrap text-base font-bold justify-start mt-4"}
-          >
-            {
-              newTags.map((tag) => (
-                <TagElement
-                  key={tag.key}
-                  name={tag.name}
-                  onDelete={() => { deleteTag(tag.key); }}
+                  <InputLabel>
+                    {"태그"}
+                  </InputLabel>
+                  <Select
+                    label={"태그"}
+                    variant={"outlined"}
+                    value={selectedTagKey}
+                    onChange={(e) => {
+                      (setSelectedTagKey as Dispatch<SetStateAction<string | number>>)(e.target.value);
+                    }}
+                  >
+                    {
+                      loadedTags
+                        .map(({ name, key }) => (<MenuItem value={key} key={key}>{name}</MenuItem>))
+                    }
+                  </Select>
+                </FormControl>
+              </article>
+              <button
+                className={"add-tag-button ml-2"}
+                onClick={addTagOnClick}
+              >
+                <AddCircleIcon
+                  style={{ color: pink[500] }}
+                  fontSize={"large"}
                 />
-              ))
-            }
-          </article>
+              </button>
+            </div>
+            <article
+              className={"flex flex-row flex-wrap text-base font-bold justify-start mt-4"}
+            >
+              {
+                newTags.map((tag) => (
+                  <TagElement
+                    key={tag.key}
+                    name={tag.name}
+                    onDelete={() => { deleteTag(tag.key); }}
+                  />
+                ))
+              }
+            </article>
+          </section>
         </section>
-      </section>
-      <section className={style.component.signIn.buttonWrapper}>
-        <button
-          className={`${style.button.base} ${style.button.colorSet.main} mt-16`}
-          onClick={confirmHandler}
-        >
-          정보 수정
-        </button>
-        <button
-          className={`${style.button.base} ${style.button.colorSet.secondary} mt-8`}
-          onClick={cancelOnClickHandler}
-        >
+        <article className={"flex flex-col mt-8"}>
+          <button
+            className={`${style.button.base} ${style.button.colorSet.main}`}
+            onClick={confirmOnClick}
+          >
+          적용
+          </button>
+          <button
+            className={`${style.button.base} ${style.button.colorSet.secondary} mt-4`}
+            onClick={cancelOnClick}
+          >
           취소
-        </button>
+          </button>
+        </article>
       </section>
     </section>
   );
